ed678a9fab37a7e2e504d938972042d35686416e
Dev-Amna <amnaharaf272@gmail.com>
2025-11-23 09:54:31 +0500

    Fix image path

--- src/components/MainAllSections/Footer.jsx
+++ src/components/MainAllSections/Footer.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import "./MainSection.css"
+import React from "react";
+import "./MainSection.css";
+import facebookIcon from "../../assets/images/icon-facebook.svg";
+import instagramIcon from "../../assets/images/icon-instagram.svg";
+import pinterestIcon from "../../assets/images/icon-pinterest.svg";
+import twitterIcon from "../../assets/images/icon-twitter.svg";
 
 function Footer() {
   return (
-   <>
-   <section className='footer'>
-    <h1>sunnyside</h1>
-    <div className="link">
-      <a href="#">About</a>
-      <a href="#">Services</a>
-      <a href="#">Projects</a>
-    </div>
-    <div className='social-links'>
-      <img src="./src/assets/images/icon-facebook.svg" alt="icon-facebook" />
-      <img src="./src/assets/images/icon-instagram.svg" alt="icon-instagram" />
-      <img src="./src/assets/images/icon-pinterest.svg" alt="icon-pinterest" />
-      <img src="./src/assets/images/icon-twitter.svg" alt="icon-twitter" />
-    </div>
-
-   </section>
-   </>
-  )
+    <section className="footer">
+      <h1>Sunnyside</h1>
+      <div className="link">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Projects</a>
+      </div>
+      <div className="social-links">
+        <img src={facebookIcon} alt="Facebook" />
+        <img src={instagramIcon} alt="Instagram" />
+        <img src={pinterestIcon} alt="Pinterest" />
+        <img src={twitterIcon} alt="Twitter" />
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/MainAllSections/TestimonialsSection.jsx
+++ src/components/MainAllSections/TestimonialsSection.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-import "./MainSection.jsx"
-import { clientsData } from "./data.js";
+import "./MainSection.css";
+import emilyImage from "../../assets/images/image-emily.jpg";
+import thomasImage from "../../assets/images/image-thomas.jpg";
+import jennieImage from "../../assets/images/image-jennie.jpg";
+
+export const clientsData = [
+  {
+    image: emilyImage,
+    content:
+      "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit",
+    name: "Emily R.",
+    role: "Marketing Director",
+  },
+  {
+    image: thomasImage,
+    content:
+      "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience",
+    name: "Thomas S.",
+    role: "Chief Operating Officer",
+  },
+  {
+    image: jennieImage,
+    content:
+      "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    name: "Jennie F.",
+    role: "Business Owner",
+  },
+];
 
 function TestimonialsSection() {
-   return (
+  return (
     <section className="TestimonialsSection">
       <h1 className="section-heading">CLIENT TESTIMONIALS</h1>
       <div className="users-row">
