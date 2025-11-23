f28bda8ab960c7aa9b295d2459b4873a7854a701
Dev-Amna <amnaharaf272@gmail.com>
2025-11-23 09:47:52 +0500

    add animation on hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./HeroSection.css";
 import arrowIcon from "../../assets/images/icon-arrow-down.svg";
+
 function HeroSection() {
+  useEffect(() => {
+    AOS.init({ duration: 1200, once: false });
+  }, []);
+
   return (
     <div className="hero-section">
       <div className="heading">
-        <h1>WE ARE CREATIVES</h1>
+        <h1 data-aos="fade-down">WE ARE CREATIVES</h1>
       </div>
+
       <img src={arrowIcon} alt="header image" />
     </div>
   );
